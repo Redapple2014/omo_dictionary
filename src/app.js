e402651693ef3cedd10ce97d94ffa8e0c7a28051
subhadip <qds-mobile@quodisys.com>
2021-08-11 10:09:10 +0530

    initial push

---
+++ src/app.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {
+   StyleSheet,
+   View,
+   Text,
+   StatusBar,
+ } from 'react-native';
+ 
+ const App = () => {
+   return (
+     <>
+       <StatusBar barStyle="dark-content" />
+       <View style={{backgroundColor:'yellow'}}>
+       </View>
+     </>
+   );
+ };
+ 
+ export default App;
+ 

7cbbe45ba77a06d35221d1ecc23d26c4c1bcc92b
qds-thang <thang@quodisys.com>
2021-08-30 20:33:44 +0700

    try to search in word table

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,25 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Platform, Image, StatusBar, Keyboard, FlatList, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  Platform,
+  Image,
+  StatusBar,
+  Keyboard,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import Constants from '../../utills/Constants';
 import Sizes from '../../utills/Size';
 import CustomSearchBar from '../../components/searchbar/CustomSearchBar';
 import AsyncStorage from '@react-native-community/async-storage';
-import { getStatusBarHeight } from "react-native-status-bar-height";
+import {getStatusBarHeight} from 'react-native-status-bar-height';
 import PouchDB from 'pouchdb-react-native';
-import d1 from '../../resources/dictionary/dict_1_small.json'
+import d1 from '../../resources/dictionary/dict_1_small.json';
 import d2 from '../../resources/dictionary/dict_2_small.json';
 import axios from 'axios';
 import * as RNLocalize from 'react-native-localize';
+PouchDB.plugin(require('pouchdb-find'));
 
 //db instance with db_name
 var localDB = new PouchDB('dev');
 
 //document list
-var toinsert = [d1,d2];
+var toinsert = [d1, d2];
 
 //lopping of all json
 toinsert.forEach(function (json) {
-
   //iterate each json and get each document
 
   // json.forEach(function (element,index){
@@ -27,28 +38,55 @@ toinsert.forEach(function (json) {
   //  // insert(element)
   // })
 
-
-
-
-  localDB.allDocs().then(entries => {
-    if(entries.rows.length==0){
-      insert(json)
-    }
-    else{
-      return
+  localDB.allDocs().then((entries) => {
+    if (entries.rows.length == 0) {
+      insert(json);
+    } else {
+      return;
     }
   });
-})
+});
 
+localDB
+  .createIndex({
+    index: {
+      fields: ['vocabularyLevel'],
+    },
+  })
+  .then(function (result) {
+    console.log(' created index origin successfully');
+    // handle result
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+localDB
+  .find({
+    selector: {vocabularyLevel: {$eq: '고급'}},
+    fields: ['_id', 'vocabularyLevel'],
+  })
+  .then(function (result) {
+    // handle result
+    console.log('result==', result);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 //insert function
 async function insert(json) {
   //bulk data insert
-  await localDB.bulkDocs(json).then(function (result) {
-    console.log('Row inserted Successfully');
-  }).catch(function (err) {
-    console.log('Unable to insert into DB. Error: ' + err.name + ' - ' + err.message);
-  });
+  await localDB
+    .bulkDocs(json)
+    .then(function (result) {
+      console.log('Row inserted Successfully');
+    })
+    .catch(function (err) {
+      console.log(
+        'Unable to insert into DB. Error: ' + err.name + ' - ' + err.message,
+      );
+    });
 
   //single document insert
   // try {
@@ -59,41 +97,39 @@ async function insert(json) {
 }
 
 const HomeScreen = () => {
-
-  const MAX_NUMBER_OF_RECENT_DATA = 3
-  const [searchText, setSearchText] = useState('')
+  const MAX_NUMBER_OF_RECENT_DATA = 3;
+  const [searchText, setSearchText] = useState('');
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
   const [reacientlySearchedData, setReacientlySearchedData] = useState([]);
   const [reacientlySearchedStatus, setReacientlySearchedStatus] = useState('');
 
-  const inputEl = useRef(null)
+  const inputEl = useRef(null);
 
   //delete recently searched data
   const removeItemValue = async function (key) {
     try {
       await AsyncStorage.removeItem(key);
-      setReacientlySearchedData([])
-      console.log('cleared')
+      setReacientlySearchedData([]);
+      console.log('cleared');
       return true;
-    }
-    catch (exception) {
+    } catch (exception) {
       return false;
     }
-  }
+  };
 
   //retrive recently searched data
   function getDatafromStorage() {
     AsyncStorage.getItem('search_data')
-      .then(req => {
+      .then((req) => {
         if (!req) {
           setReacientlySearchedStatus('No recent search data avalible');
           //console.log('no data found on recent search')
-          return
+          return;
         }
-        setReacientlySearchedData(JSON.parse(req))
+        setReacientlySearchedData(JSON.parse(req));
         //console.log(JSON.parse(req))
       })
-      .catch(error => console.log('error!'));
+      .catch((error) => console.log('error!'));
   }
 
   //search the entered data
@@ -102,137 +138,210 @@ const HomeScreen = () => {
       if (reacientlySearchedData.length > MAX_NUMBER_OF_RECENT_DATA) {
         reacientlySearchedData.splice(MAX_NUMBER_OF_RECENT_DATA, 1);
       }
-      AsyncStorage.setItem('search_data', JSON.stringify([searchText, ...reacientlySearchedData]))
-      getDatafromStorage()
-      setSearchText(searchText)
-    }
-    else {
-      console.log('search text input is empty')
-      setSearchText('')
+      AsyncStorage.setItem(
+        'search_data',
+        JSON.stringify([searchText, ...reacientlySearchedData]),
+      );
+      getDatafromStorage();
+      setSearchText(searchText);
+    } else {
+      console.log('search text input is empty');
+      setSearchText('');
     }
   }
 
   useEffect(() => {
-
     //destroy db
     // try{
     //   localDB.destroy()
     // }catch(e){
     //   console.log(e)
     // }
-    
 
     getDatafromStorage();
     const keyboardDidShowListener = Keyboard.addListener(
       'keyboardDidShow',
       () => {
         setKeyboardVisible(true);
-      }
+      },
     );
     const keyboardDidHideListener = Keyboard.addListener(
       'keyboardDidHide',
       () => {
         setKeyboardVisible(false);
-      }
+      },
     );
 
     return () => {
       keyboardDidHideListener.remove();
       keyboardDidShowListener.remove();
-
     };
   }, []);
 
-
   //fetch data by id
-async function fetchDataById(){
-  const id = '72336'
-  localDB.get(id).then(function (doc) {
-   console.log(`data : ${id} `,JSON.stringify(doc))
-  }).catch(function (err) {
-    console.log(err);
-  });
+  async function fetchDataById() {
+    const id = '72336';
+    localDB
+      .get(id)
+      .then(function (doc) {
+        console.log(`data : ${id} `, JSON.stringify(doc));
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
 
-// get all documents
-  // localDB.allDocs().then(function (result) {
-  //   console.log(JSON.stringify('************ ',result))
-  //   }).catch(function (err) {
-  //     console.log(err);
-  //   });
-}
+    // get all documents
+    // localDB.allDocs().then(function (result) {
+    //   console.log(JSON.stringify('************ ',result))
+    //   }).catch(function (err) {
+    //     console.log(err);
+    //   });
+  }
 
-
-useEffect(()=>{
-
-   //fetchDataById()
-},[])
-
+  useEffect(() => {
+    //fetchDataById()
+  }, []);
 
   //render recently rearched data
   function renderRecentSearchData() {
     return (
       <FlatList
         keyboardShouldPersistTaps={'handled'}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => setSearchText(item)
-            }>
-            <View style={{ backgroundColor: 'white', flexDirection: 'row', height: 45, borderBottomWidth: .5, borderColor: Constants.appColors.LIGHTGRAY, alignItems: 'center', paddingHorizontal: 8, borderWidth: .5 }}>
-              <Text style={{ fontSize: 16, color: Constants.appColors.GRAY, paddingLeft: 12 }}>{item}</Text>
+        renderItem={({item, index}) => (
+          <TouchableOpacity key={index} onPress={() => setSearchText(item)}>
+            <View
+              style={{
+                backgroundColor: 'white',
+                flexDirection: 'row',
+                height: 45,
+                borderBottomWidth: 0.5,
+                borderColor: Constants.appColors.LIGHTGRAY,
+                alignItems: 'center',
+                paddingHorizontal: 8,
+                borderWidth: 0.5,
+              }}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: Constants.appColors.GRAY,
+                  paddingLeft: 12,
+                }}>
+                {item}
+              </Text>
             </View>
           </TouchableOpacity>
-
         )}
         keyExtractor={(item, index) => index.toString()}
         data={reacientlySearchedData}
         numColumns={1}
         showsVerticalScrollIndicator={false}
       />
-    )
+    );
   }
 
-
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ backgroundColor: Constants.appColors.PRIMARY_COLOR, paddingTop: Platform.OS == "ios" ? getStatusBarHeight() : 0 }}>
-        <StatusBar barStyle="light-content" backgroundColor={Constants.appColors.PRIMARY_COLOR} />
+    <View style={{flex: 1}}>
+      <View
+        style={{
+          backgroundColor: Constants.appColors.PRIMARY_COLOR,
+          paddingTop: Platform.OS == 'ios' ? getStatusBarHeight() : 0,
+        }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={Constants.appColors.PRIMARY_COLOR}
+        />
       </View>
-      <View style={{ backgroundColor: 'red',justifyContent:'center', height: isKeyboardVisible || searchText.length > 0 ? Sizes.WINDOW_HEIGHT * .1 : Sizes.WINDOW_HEIGHT * 0.38, backgroundColor: Constants.appColors.PRIMARY_COLOR, justifyContent: 'center', alignItems: 'center' }}>
-        {isKeyboardVisible || searchText.length > 0 ? <></> : <View style={{marginBottom:Sizes.WINDOW_WIDTH*.18}}><Image source={require('../../assets/logo/omo-logo_1.png')} style={{ width: 300, height: 100, resizeMode: 'contain' }} /></View>}
+      <View
+        style={{
+          backgroundColor: 'red',
+          justifyContent: 'center',
+          height:
+            isKeyboardVisible || searchText.length > 0
+              ? Sizes.WINDOW_HEIGHT * 0.1
+              : Sizes.WINDOW_HEIGHT * 0.38,
+          backgroundColor: Constants.appColors.PRIMARY_COLOR,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {isKeyboardVisible || searchText.length > 0 ? (
+          <></>
+        ) : (
+          <View style={{marginBottom: Sizes.WINDOW_WIDTH * 0.18}}>
+            <Image
+              source={require('../../assets/logo/omo-logo_1.png')}
+              style={{width: 300, height: 100, resizeMode: 'contain'}}
+            />
+          </View>
+        )}
       </View>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <CustomSearchBar
           ref={inputEl}
           lightTheme
           value={searchText}
-          onChangeText={value => setSearchText(value)}
-          inputContainerStyle={{ backgroundColor: Constants.appColors.WHITE, height: 48, borderRadius: 10, top: -1 }}
-          containerStyle={{ padding: 0, margin: 0, borderRadius: 18, height: 45, width: '95%', top: isKeyboardVisible ? Sizes.WINDOW_HEIGHT * 0.01 : searchText.length > 0 ? Sizes.WINDOW_HEIGHT * 0.01 : Sizes.WINDOW_HEIGHT * 0.3, position: 'absolute', alignSelf: 'center' }}
-          inputStyle={{ color: 'black' }}
-          placeholder='Search in Korean or English'
+          onChangeText={(value) => setSearchText(value)}
+          inputContainerStyle={{
+            backgroundColor: Constants.appColors.WHITE,
+            height: 48,
+            borderRadius: 10,
+            top: -1,
+          }}
+          containerStyle={{
+            padding: 0,
+            margin: 0,
+            borderRadius: 18,
+            height: 45,
+            width: '95%',
+            top: isKeyboardVisible
+              ? Sizes.WINDOW_HEIGHT * 0.01
+              : searchText.length > 0
+              ? Sizes.WINDOW_HEIGHT * 0.01
+              : Sizes.WINDOW_HEIGHT * 0.3,
+            position: 'absolute',
+            alignSelf: 'center',
+          }}
+          inputStyle={{color: 'black'}}
+          placeholder="Search in Korean or English"
           onSubmitEditing={onSearchSubmit}
         />
       </TouchableWithoutFeedback>
-      {
-        isKeyboardVisible && reacientlySearchedData.length != 0 ? <>
-          <View style={{ marginVertical: 8, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 8 }}>
-            <Text style={{ fontWeight: 'bold', }}>Recently Searched</Text>
-            <TouchableOpacity onPress={() =>
-              removeItemValue('search_data')
-            }>
-              <Text style={{ fontWeight: '400', textDecorationLine: 'underline' }}>Clear History</Text>
+      {isKeyboardVisible && reacientlySearchedData.length != 0 ? (
+        <>
+          <View
+            style={{
+              marginVertical: 8,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingHorizontal: 8,
+            }}>
+            <Text style={{fontWeight: 'bold'}}>Recently Searched</Text>
+            <TouchableOpacity onPress={() => removeItemValue('search_data')}>
+              <Text
+                style={{fontWeight: '400', textDecorationLine: 'underline'}}>
+                Clear History
+              </Text>
             </TouchableOpacity>
           </View>
           {renderRecentSearchData()}
-        </> : reacientlySearchedData.length == 0 && isKeyboardVisible ? <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}><Text style={{ paddingTop: 16 }}>{reacientlySearchedStatus}</Text></View> : <></>
-      }
+        </>
+      ) : reacientlySearchedData.length == 0 && isKeyboardVisible ? (
+        <View
+          style={{
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{paddingTop: 16}}>{reacientlySearchedStatus}</Text>
+        </View>
+      ) : (
+        <></>
+      )}
     </View>
-  )
-}
+  );
+};
 
 HomeScreen.navigationOptions = {
-  headerShown: false
-}
+  headerShown: false,
+};
 
 export default HomeScreen;

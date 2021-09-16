be6f22cc2f7e5bc989f08cbef201c460cb0f4b2d
qds-thang <thang@quodisys.com>
2021-09-16 11:09:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 /**
  * @format
  */
-<<<<<<< HEAD
-=======
-
 import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
->>>>>>> try-sqlite-adapter
 import {AppRegistry} from 'react-native';
 import App from './src/app';
 import {name as appName} from './app.json';
